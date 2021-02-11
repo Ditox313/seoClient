b0207ea846682d2a1c7bb3039862390b3d343412
Xsodex <flex513513@gmail.com>
2021-02-12 01:09:15 +0300

    adaptive tabs js

--- js/dist/common.dev.js
+++ js/dist/common.dev.js
@@ -118,7 +118,7 @@ $('.block_5__more').on('click', function () {
   $('.tab_uslugi_toggle_mob').slideToggle('normal');
   $('.block_5__more span, .block_5__more svg').toggleClass('dn');
   $('.more_2').toggleClass('dn');
-}); // Tabs
+}); // Tabs desc
 
 var tabs = document.querySelectorAll('.tab'),
     menu_links = document.querySelectorAll('.block_5__tabmenu ul li'),
@@ -160,4 +160,58 @@ function clickMenu() {
 
 hideTabs();
 showTab();
-clickMenu();
+clickMenu(); // select
+// Select
+
+$('.select').each(function () {
+  // Variables
+  var $this = $(this),
+      selectOption = $this.find('option'),
+      selectOptionLength = selectOption.length,
+      selectedOption = selectOption.filter(':selected'),
+      dur = 500;
+  $this.hide(); // Wrap all in select box
+
+  $this.wrap('<div class="select"></div>'); // Style box
+
+  $('<div>', {
+    "class": 'select__gap',
+    text: 'Форма сотрудничества'
+  }).insertAfter($this);
+  var selectGap = $this.next('.select__gap'),
+      caret = selectGap.find('.caret'); // Add ul list
+
+  $('<ul>', {
+    "class": 'select__list'
+  }).insertAfter(selectGap);
+  var selectList = selectGap.next('.select__list'); // Add li - option items
+
+  for (var i = 0; i < selectOptionLength; i++) {
+    $('<li>', {
+      "class": 'select__item',
+      html: $('<span>', {
+        text: selectOption.eq(i).text()
+      })
+    }).attr('data-value', selectOption.eq(i).val()).appendTo(selectList);
+  } // Find all items
+
+
+  var selectItem = selectList.find('li');
+  selectList.slideUp(0);
+  selectGap.on('click', function () {
+    if (!$(this).hasClass('on')) {
+      $(this).addClass('on');
+      selectList.slideDown(dur);
+      selectItem.on('click', function () {
+        var chooseItem = $(this).data('value');
+        $('select').val(chooseItem).attr('selected', 'selected');
+        selectGap.text($(this).find('span').text());
+        selectList.slideUp(dur);
+        selectGap.removeClass('on');
+      });
+    } else {
+      $(this).removeClass('on');
+      selectList.slideUp(dur);
+    }
+  });
+});
